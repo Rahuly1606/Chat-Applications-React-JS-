f4bf4274bb1f1ecc821d09bad8ca9578d474db8a
Rahul kumar <alexrahul9576@gmail.com>
2025-01-06 00:02:41 +0530

    message model, message controller and routes are created

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const signup = async(req,res)=>{
         }
     } catch (error) {
         console.log("Signup error:",error);
-        res.status(500).json({message:"Something went wrong"});
+        res.status(500).json({error:"Something went wrong"});
         
     }
 };
@@ -68,7 +68,7 @@ export const login = async(req,res)=>{
 
     } catch (error) {
         console.log("Error in login:",error.message);
-        res.status(500).json({message:"Something went wrong"});
+        res.status(500).json({error:"Something went wrong"});
         
     }
 };
@@ -100,7 +100,7 @@ export const updateProfile = async(req,res)=>{
     res.status(200).json(updatedUser)
    } catch (error) {
     console.log("Error in Update profile:",error.message);
-    res.status(500).json({message:"Something went wrong"});
+    res.status(500).json({error:"Something went wrong"});
    }
 }
 
@@ -109,6 +109,6 @@ export const checkAuth = (req,res)=>{
         res.status(200).json(req.user);
     } catch (error) {
         console.log("Error in checkAuth controller:",error.message);
-        res.status(500).json({message:"Something went wrong"});
+        res.status(500).json({error:"Something went wrong"});
     }
 }
