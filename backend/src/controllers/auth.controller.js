d0a6fa39749c88bf3032493596887b9be013d589
Rahul kumar <alexrahul9576@gmail.com>
2025-01-05 22:40:19 +0530

    backend for authentication completed (login, singup, logout, check auth)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async(req,res)=>{
     const  {email, fullName, password} = req.body;
@@ -43,9 +44,71 @@ export const signup = async(req,res)=>{
         
     }
 };
-export const login = (req,res)=>{
-    res.send("Signup route");
+
+export const login = async(req,res)=>{
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email}); 
+        if(!user){
+            return res.status(400).json({message:"Invalid email"});
+        }
+       const isPasswordCorrect =  await bcrypt.compare(password, user.password)
+         if(!isPasswordCorrect){
+              return res.status(400).json({message:"Invalid password"});
+         }
+         generateToken(user._id,res);
+
+         res.status(200).json({
+            _id:user._id,    
+            fullName : user.fullName,
+            email  : user.email,
+            profilePic : user.profilePic
+        });
+
+    } catch (error) {
+        console.log("Error in login:",error.message);
+        res.status(500).json({message:"Something went wrong"});
+        
+    }
 };
-export const logout = (req,res)=>{
-    res.send("Signup route");
+
+export const logout = (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in Logout:", error.message);
+        res.status(500).json({ message: "Something went wrong" });
+    }
 };
+
+export const updateProfile = async(req,res)=>{
+    
+   try {
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+    if(!profilePic){
+        return res.status(400).json({message:"Profile Pic is required"});
+    }
+    const uploadedResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadedResponse.secure_url},{new:true});
+    res.status(200).json(updatedUser)
+   } catch (error) {
+    console.log("Error in Update profile:",error.message);
+    res.status(500).json({message:"Something went wrong"});
+   }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller:",error.message);
+        res.status(500).json({message:"Something went wrong"});
+    }
+}
