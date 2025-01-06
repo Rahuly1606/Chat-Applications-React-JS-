b8d4e8e29bdd87efb77473725cc8c81e2ad1457e
Rahul kumar <alexrahul9576@gmail.com>
2025-01-06 13:53:27 +0530

    message sending and receiving part done

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,29 +1,92 @@
-import { MessageSquare } from "lucide-react";
+import { useChatStore } from "../store/useChatStore";
+import { useEffect, useRef } from "react";
 
-const NoChatSelected = () => {
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
+
+const ChatContainer = () => {
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    // subscribeToMessages,
+    // unsubscribeFromMessages,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+
+//     subscribeToMessages();
+
+//     return () => unsubscribeFromMessages();
+  }, [selectedUser._id, getMessages]);
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
   return (
-    <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
-      <div className="max-w-md text-center space-y-6">
-        {/* Icon Display */}
-        <div className="flex justify-center gap-4 mb-4">
-          <div className="relative">
-            <div
-              className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
-             justify-center animate-bounce"
-            >
-              <MessageSquare className="w-8 h-8 text-primary " />
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) => (
+          <div
+            key={message._id}
+            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
+          >
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    message.senderId === authUser._id
+                      ? authUser.profilePic || "/avatar.jpg"
+                      : selectedUser.profilePic || "/avatar.jpg"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(message.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {message.image && (
+                <img
+                  src={message.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {message.text && <p>{message.text}</p>}
             </div>
           </div>
-        </div>
-
-        {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
-        <p className="text-base-content/60">
-          Select a conversation from the sidebar to start chatting
-        </p>
+        ))}
       </div>
+
+      <MessageInput />
     </div>
   );
 };
-
-export default NoChatSelected;
+export default ChatContainer;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -3,7 +3,8 @@ import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 import axios from "axios";
 
-export const useChatStore = create((set)=>({
+
+export const useChatStore = create((set, get)=>({
     messages :[],
     users : [],
     selectedUser : null,
@@ -22,7 +23,7 @@ export const useChatStore = create((set)=>({
         }
       },
 
-      
+
       getMessages: async (userId) => {
         set({ isMessagesLoading: true });
         try {
@@ -36,4 +37,36 @@ export const useChatStore = create((set)=>({
       },
 
 
+      sendMessage: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+          const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+          set({ messages: [...messages, res.data] });
+        } catch (error) {
+          toast.error(error.response.data.message);
+        }
+      },
+    
+    //   subscribeToMessages: () => {
+    //     const { selectedUser } = get();
+    //     if (!selectedUser) return;
+    
+    //     const socket = useAuthStore.getState().socket;
+    
+    //     socket.on("newMessage", (newMessage) => {
+    //       const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+    //       if (!isMessageSentFromSelectedUser) return;
+    
+    //       set({
+    //         messages: [...get().messages, newMessage],
+    //       });
+    //     });
+    //   },
+    
+    //   unsubscribeFromMessages: () => {
+    //     const socket = useAuthStore.getState().socket;
+    //     socket.off("newMessage");
+    //   },
+    
+      setSelectedUser: (selectedUser) => set({ selectedUser }),
 }))
