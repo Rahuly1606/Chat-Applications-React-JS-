fe767edb3b5e17377dfc7158884533daeab41b78
Rahul Kumar <163739563+Rahuly1606@users.noreply.github.com>
2025-01-06 17:06:45 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ import cors from "cors"
 import { app, server } from './lib/socket.js';
 import path from "path";
 
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 
@@ -28,14 +30,11 @@ app.use("/api/messages",messageRoutes);
 
 
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Use `__dirname` as usual
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 server.listen(PORT,()=>{
     console.log('Server is running on port:'+PORT);
     connectDb();
