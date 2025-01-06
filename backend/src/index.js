34db43da262bbe4db52669e65a7740210d3326d2
Rahul kumar <alexrahul9576@gmail.com>
2025-01-06 17:19:09 +0530

    fixed some issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,12 @@ import cookieParser from 'cookie-parser';
 import cors from "cors"
 import { app, server } from './lib/socket.js';
 import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT  = process.env.PORT;
 
@@ -27,14 +30,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT,()=>{
     console.log('Server is running on port:'+PORT);
