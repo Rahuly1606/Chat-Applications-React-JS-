6564453d451388ad3e5fc549ca1beccc6076e2d5
Rahul kumar <alexrahul9576@gmail.com>
2025-01-06 16:46:03 +0530

    fixed some issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,6 +26,8 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
+
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
