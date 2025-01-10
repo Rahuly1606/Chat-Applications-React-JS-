3907b2c98b127281d36bb71fe802f143c7a019e6
Rahul kumar <alexrahul9576@gmail.com>
2025-01-10 13:56:40 +0530

    did some changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -17,7 +17,7 @@ export const signup = async(req,res)=>{
         if (user){
             return res.status(400).json({message:"User already exists"});
         }
-        const salt = await bcrypt.genSalt(10); // Fix: await the bcrypt.genSalt method
+        const salt = await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password, salt);
 
         const newUser  = new User({
